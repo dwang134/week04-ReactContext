40aa4206189e3f4c3673d9f8b0247708e4bc7a65
DEELIN WONG <dc.wang547@gmail.com>
2022-11-30 19:22:40 -0500

    home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Games from '../components/Games'
+import {Button} from '@chakra-ui/react';
+import Form from '../components/Form';
 
 const Home = () => {
 
+  const [adding, setAdding]= useState(false);
 
   return (
     <div>
     <h1>World Cup Games</h1>
-    <h2>Add new game</h2>
+    <Button onClick= {()=> setAdding(true)}>Add more games</Button>
+    {adding && (
+      <Form/>
+    )}
     <Games/>
     </div>
   )
